a306d64a99ae7e4b6176550e71e8e5ae4889b8cc
victors3136 <victor.selegean@stud.ubbcluj.ro>
2024-10-12 16:08:44 +0300

    Updating & Logging

--- main.cpp
+++ main.cpp
@@ -35,14 +35,19 @@ class SensorSystem {
 private:
     const size_t size;
     std::vector<int> variables;
-    const std::vector<std::vector<int>> dependencies;
-    const std::vector<std::vector<int>> dependents;
+    const std::vector<std::vector<size_t>> dependencies;
+    const std::vector<std::vector<size_t>> dependents;
     std::vector<std::unique_ptr<std::mutex>> locks;
     std::vector<std::thread> threads;
-    std::osyncstream logger;
+    static constexpr int WORKER_ITER_COUNT = 20;
+    static constexpr int WORKER_MAX_SLEEP_TIME_MS = 5000;
+    static constexpr int CC_ITER_COUNT = 30;
+    static constexpr int CC_MAX_SLEEP_TIME_MS = 1000;
+    static constexpr int UPDATE_RANGE = 20;
+    static constexpr int UPDATE_MEAN_VALUE = 10;
 
     auto computeDependents() {
-        std::vector<std::vector<int>> inverseDependencies{};
+        std::vector<std::vector<size_t>> inverseDependencies{};
         inverseDependencies.reserve(size);
         for (auto dependentIndex = 0; dependentIndex < size; ++dependentIndex) {
             inverseDependencies.emplace_back();
@@ -57,19 +62,43 @@ private:
         return inverseDependencies;
     }
 
-    auto createLocks() {
+    [[nodiscard]] auto createLocks() const {
         std::vector<std::unique_ptr<std::mutex>> mutexVector;
-        mutexVector.reserve(dependencies.size());
-        for (auto i = 0; i < dependencies.size(); ++i) { mutexVector.emplace_back(std::make_unique<std::mutex>()); }
+        mutexVector.reserve(size);
+        for (auto i = 0; i < size; ++i) { mutexVector.emplace_back(std::make_unique<std::mutex>()); }
         return mutexVector;
     }
 
-    static auto search(size_t startID, const std::vector<std::vector<int>> &searchSpace) {
+    [[nodiscard]] auto createVariables() const {
+        std::vector<int> variableVector;
+        variableVector.resize(size, 0);
+        return variableVector;
+    }
+
+    static auto search(size_t startID, const std::vector<std::vector<size_t>> &searchSpace) {
         std::set<size_t> visited{startID};
         std::vector<size_t> result{startID};
         // Impure function that modifies the 'visited' and 'result' objects
         std::function<void(size_t)> dfs = [&](int var) {
-            for (int dep: searchSpace[var]) {
+            for (auto dep: searchSpace[var]) {
+                result.push_back(dep);
+                if (visited.find(dep) == visited.end()) {
+                    visited.insert(dep);
+                    dfs(dep);
+                }
+            }
+        };
+        dfs(startID);
+        std::sort(result.begin(), result.end());
+        return result;
+    }
+
+    static auto uniqueSearch(size_t startID, const std::vector<std::vector<size_t>> &searchSpace) {
+        std::set<size_t> visited{startID};
+        std::vector<size_t> result{startID};
+        // Impure function that modifies the 'visited' and 'result' objects
+        std::function<void(size_t)> dfs = [&](int var) {
+            for (auto dep: searchSpace[var]) {
                 if (visited.find(dep) == visited.end()) {
                     visited.insert(dep);
                     dfs(dep);
@@ -82,29 +111,21 @@ private:
         return result;
     }
 
-
-    // Traverse the dependency tree and gather all direct and indirect dependencies of the variable with said id
-    // The return vector is sorted in order to avoid deadlocks
-    // Does not need to be synchronised as of now, since the dependency matrix is constant across the program's runtime
-    auto getAllDependencies(int variableID) {
-        assert(variableID < size);
-        return search(variableID, dependencies);
-    }
-
     // Traverse the dependents tree and gather all direct and indirect dependencies of the variable with said id
     // The return vector is sorted in order to avoid deadlocks
     // Does not need to be synchronised as of now, since the dependency matrix is constant across the program's runtime
-    auto getAllDependents(size_t variableID) {
-        assert(variableID < size);
-        return search(variableID, dependents);
+    [[nodiscard]] auto getAllDependents(size_t variableID) const {
+        assert(variableID < size && "Trying to read dependents of a variable that is not part of the system");
+        return uniqueSearch(variableID, dependents);
     }
 
-    auto updateVariable(size_t variableID, int delta) {
-        assert(variableID < size && "Trying to update a variable that is not part of the system");
-        assert(dependencies[variableID].empty() && "Trying to update a non-primary variable");
-        std::cout << "[Thread " << std::this_thread::get_id() << "] Update " << variableID << " by " << delta
-                  << std::endl;
-        const auto variablesDependents = getAllDependents(variableID);
+    auto updateVariable(size_t variableId, int delta) {
+        assert(variableId < size && "Trying to update a variable that is not part of the system");
+        assert(dependencies[variableId].empty() && "Trying to update a non-primary variable");
+        std::osyncstream(std::cout) << "[Thread " << std::this_thread::get_id() << "] Update " << variableId << " by "
+                                    << delta
+                                    << std::endl;
+        const auto variablesDependents = getAllDependents(variableId);
         // for all variables that depend on our current variable, gather their locks and use a lock guard to
         // lock them all at once and update their values
         std::vector<std::unique_lock<std::mutex>> lockGuards;
@@ -112,16 +133,18 @@ private:
         for (const auto dep: variablesDependents) {
             lockGuards.emplace_back(*locks[dep]);
         }
-
+        for (const auto id: search(variableId, dependents)) {
+            variables[id] += delta;
+        }
     }
 
-    auto checkConsistency() {
+    auto checkConsistency() const {
         std::vector<std::unique_lock<std::mutex>> lockGuards;
         lockGuards.reserve(variables.size());
         for (int index = 0; index < size; ++index) {
             lockGuards.emplace_back(*locks[index]);
         }
-        std::cout << "[CC] Starting" << std::endl;
+        std::osyncstream(std::cout) << "[CC] Starting" << std::endl;
         for (int index = 0; index < size; ++index) {
             const auto expectedValue = std::accumulate(dependencies[index].cbegin(),
                                                        dependencies[index].cend(),
@@ -130,62 +153,54 @@ private:
                                                            return partialSum + variables[valueId];
                                                        });
             const auto actualValue = variables[index];
-            assert(expectedValue == actualValue);
+            assert(expectedValue == actualValue && "[CC] Failure");
         }
-        std::cout << "[CC] Success" << std::endl;
+        std::osyncstream(std::cout) << "[CC] Success" << std::endl;
     }
 
 public:
-    explicit SensorSystem(const std::vector<std::vector<int>> &&deps) : dependencies(deps),
-                                                                        dependents(computeDependents()),
-                                                                        locks(createLocks()),
-                                                                        size(deps.size()),
-                                                                        logger(std::cout) {
-//        std::cout << "Size              " << size << std::endl;
-//        std::cout << "Dependencies Size " << dependencies.size() << std::endl;
-//        std::cout << "Dependents Size   " << dependents.size() << std::endl;
-//        std::cout << "Locks Size        " << locks.size() << std::endl;
-        assert(size == dependencies.size());
-        assert(size == dependents.size());
-        assert(size == locks.size());
-        variables.resize(size, 0);
+    explicit SensorSystem(const std::vector<std::vector<size_t>> &&deps) : dependencies(deps),
+                                                                           dependents(computeDependents()),
+                                                                           locks(createLocks()),
+                                                                           variables(createVariables()),
+                                                                           size(deps.size()) {
+        assert(size == variables.size() && "Mismatch between variable vector size and system size");
+        assert(size == dependencies.size() && "Mismatch between dependencies vector size and system size");
+        assert(size == dependents.size() && "Mismatch between dependents vector size and system size");
+        assert(size == locks.size() && "Mismatch between locks vector size and system size");
     }
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "cert-msc50-cpp"
 
-    // Now we need a function that spawns threads. There should be about as many threads as input variables
-    // (variables with no dependencies)
-    // The thread should execute a function that should be a member variable of the class
     auto startThreads() {
         static auto baseSensorsCount = std::count_if(dependencies.cbegin(),
                                                      dependencies.cend(),
                                                      [](const auto &dependencyVector) { return dependencyVector.empty(); });
-        std::cout << "[Main] Starting worker threads" << std::endl;
+        std::osyncstream(std::cout) << "[Main] Starting worker threads" << std::endl;
         threads.reserve(baseSensorsCount + 1);
         for (int index = 0; index < baseSensorsCount; ++index) {
             threads.emplace_back([this]() {
-                std::cout << "[Thread " << std::this_thread::get_id() << "] start" << std::endl;
+                std::osyncstream(std::cout) << "[Thread " << std::this_thread::get_id() << "] Start" << std::endl;
                 std::srand(std::chrono::system_clock::now().time_since_epoch().count());
                 int i = 0;
-                while (++i < 20) {
+                while (++i < WORKER_ITER_COUNT) {
                     const auto variableId = std::rand() % size;
                     if (dependencies[variableId].empty()) {
-                        updateVariable(variableId, std::rand() % 20 - 10);
-                        // sleep for at most 5 seconds
-                        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 5000));
+                        updateVariable(variableId, std::rand() % UPDATE_RANGE - UPDATE_MEAN_VALUE);
+                        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % WORKER_MAX_SLEEP_TIME_MS));
                     }
                 }
-                std::cout << "[Thread " << std::this_thread::get_id() << "] end" << std::endl;
+                std::osyncstream(std::cout) << "[Thread " << std::this_thread::get_id() << "] End" << std::endl;
             });
         }
         threads.emplace_back(/*Consistency checker*/
                 [this]() {
                     std::srand(std::chrono::system_clock::now().time_since_epoch().count());
                     int i = 0;
-                    while (++i < 10) {
+                    while (++i < CC_ITER_COUNT) {
                         checkConsistency();
-                        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % 1000));
+                        std::this_thread::sleep_for(std::chrono::milliseconds(std::rand() % CC_MAX_SLEEP_TIME_MS));
                     }
                 });
     }
@@ -193,31 +208,30 @@ public:
 #pragma clang diagnostic pop
 
     auto gatherThreads() {
-        std::cout << "[Main] waiting for workers" << std::endl;
+        std::osyncstream(std::cout) << "[Main] waiting for workers" << std::endl;
         for (auto &thread: threads) {
             if (thread.joinable()) { thread.join(); }
         }
-        std::cout << "[Main] gathered threads" << std::endl;
+        std::osyncstream(std::cout) << "[Main] gathered threads" << std::endl;
         checkConsistency();
     }
 
 };
 
 int main() {
-    std::vector<std::vector<int>> dependencies = {
-            {},
-            {},
-            {},
-            {},
-            {},
-            {1, 2},
-            {4, 3, 0},
-            {},
-            {7, 6},
-            {2},
-            {8, 9, 5}
-    };
-    SensorSystem system(std::move(dependencies));
+    SensorSystem system({
+                                {}          /*  0 */,
+                                {}          /*  1 */,
+                                {}          /*  2 */,
+                                {}          /*  3 */,
+                                {}          /*  4 */,
+                                {1, 2}      /*  5 */,
+                                {4, 3, 0}   /*  6 */,
+                                {}          /*  7 */,
+                                {7, 6}      /*  8 */,
+                                {2}         /*  9 */,
+                                {8, 9, 5}   /* 10 */
+                        });
     system.startThreads();
     system.gatherThreads();
     return 0;
